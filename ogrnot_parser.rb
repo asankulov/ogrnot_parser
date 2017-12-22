a3e5310f96ccacdbe54bd05fad742548f07bc60f
Kylych Asankulov <asankulov.kylych@gmail.com>
2017-12-22 14:34:57 +0600

    uncomment whole code

--- ogrnot_parser.rb
+++ ogrnot_parser.rb
@@ -4,23 +4,21 @@ require 'nokogiri'
 
 Selenium::WebDriver::Chrome.driver_path = Dir.pwd + '/lib/browsers/chromedriver'
 
-# username = '1404.01016'
-# password = '1'
+username = '1xxx.xxxxx'
+password = 'your_password'
 
-# browser = Watir::Browser.new :chrome, headless: true, 'disable-gpu': false
-# browser.goto('http://ogrnot.manas.edu.kg/')
-#
-# browser.input(:name => 'frm_kullanici').to_subtype.clear
-# browser.input(:name => 'frm_kullanici').send_keys '1404.01016'
-# browser.input(:name => 'frm_sifre').to_subtype.clear
-# browser.input(:name => 'frm_sifre').send_keys '1'
-# browser.input(:value => ' Giriş ').click
-# browser.button(:name => 'frm_not').click
-#
-#
-# open('ogrnot.html', 'w') do |f|
-# f.puts browser.html
-#   end
+browser = Watir::Browser.new :chrome, headless: true, 'disable-gpu': false
+browser.goto('http://ogrnot.manas.edu.kg/')
+browser.input(:name => 'frm_kullanici').to_subtype.clear
+browser.input(:name => 'frm_kullanici').send_keys username
+browser.input(:name => 'frm_sifre').to_subtype.clear
+browser.input(:name => 'frm_sifre').send_keys password
+browser.input(:value => ' Giriş ').click
+browser.button(:name => 'frm_not').click
 
-# browser.button(:name => 'frm_cikis').click
+
+open('ogrnot.html', 'w') do |f|
+f.puts browser.html
+  end
+browser.button(:name => 'frm_cikis').click
 
